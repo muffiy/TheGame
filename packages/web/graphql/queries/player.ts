52546de75dcce39a948946f6ae1dbd3d14155563
Seroxdesign <sherifcherfa@gmail.com>
2023-12-14 08:48:49 -0500

    hasura stuff for pinning

--- packages/web/graphql/queries/player.ts
+++ packages/web/graphql/queries/player.ts
@@ -16,11 +16,32 @@ const getPlayerLinksQuery = /* GraphQL */ `
   }
 `;
 
+// questchainId returns as a string `{questchain.address}-{questchain.title}`
+const getPlayerPinnedQuestchainsQuery = /* GraphQL */ `
+  query GetPlayerPinnedQuestchains($playerId: uuid!) {
+    pinned_questchains(where: { player_id: { _eq: $playerId } }) {
+      id
+      questchain_id
+    }
+  }
+`
+
 export const getPlayerLinks = async (playerId: string) => {
   if (!playerId) throw new Error('Missing Player Id');
   const { data } = await client
     .query<GetPlayerLinksQuery, GetPlayerLinksQueryVariables>(
       getPlayerLinksQuery,
+      { playerId },
+    )
+    .toPromise();
+  return data;
+};
+
+export const getPlayerPinnedQuestchains = async (playerId: string) => {
+  if (!playerId) throw new Error('Missing Player Id');
+  const { data } = await client
+    .query<GetPlayerLinksQuery, GetPlayerLinksQueryVariables>(
+      getPlayerPinnedQuestchainsQuery,
       { playerId },
     )
     .toPromise();
